60b845024c0ad0392b6c1989f5f5d5de60f7a722
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-02-02 15:55:43 -0500

    now with footprint function

--- pwnnyquist/header_time.py
+++ pwnnyquist/header_time.py
@@ -3,37 +3,44 @@ import pyfits
 from astropy.time import Time
 import glob
 import matplotlib.pyplot as plt
-from sin_tests import fit_sine, show_sine
 
-def get_times(D, kid):
-    qs = range(17)
+def get_times(fnames):
+    """
+    # `get_times`
+
+    Take a list of fits file names (in order of ascending quarter
+    and read the relevent information from the header.
+    """
+
     t_bjd, diff = [], []
-    for q in qs:
-        # load fits file
-        fname = glob.glob("%s/Q%s_public/kplr%s-*_llc.fits" % (D, q, kid.zfill(9)))
-        hdulist = pyfits.open(fname[0])
+    for fname in fnames:
+        hdulist = pyfits.open(fname)
         hdr = hdulist[1].header
-
         # read times in header
         bjdrefi = hdr["BJDREFI"]  # integer part of time = 2454833
         tstart = hdr["TSTART"]  # observation start time in BJD-BJDREF
         tstop = hdr["TSTOP"]  # observation stop time in BJD-BJDREF
         date_obs = hdr["DATE-OBS"]  # TSTART as UTC calendar date
         date_end = hdr["DATE-END"]  # TSTOP as UTC calendar date
-
+        dt = hdr["TIMEDEL"]
         # format dates
         iso_tstrt = " ".join((date_obs[:10], date_obs[11:23]))
         iso_tstp = " ".join((date_end[:10], date_end[11:23]))
         tstrt = Time(iso_tstrt, format="iso", scale="utc").jd
         tstp = Time(iso_tstp, format="iso", scale="utc").jd
-
         t_bjd.append(tstart)
         t_bjd.append(tstop)
         diff.append(tstrt - bjdrefi - tstart)  # utc - bjd
         diff.append(tstp - bjdrefi - tstop)  # utc - bjd
-    return np.array(t_bjd), np.array(diff)
+    return np.array(t_bjd), np.array(diff), dt
 
 def fit_sine_and_linear(x, y, w):
+    """
+    # `fit_sine_and_linear`
+
+    Fit a sum of sines + cosines plus a linear trend
+    """
+
     M = np.ones((len(x), 4))
     M[:, 0] = np.sin(w*x)
     M[:, 1] = np.cos(w*x)
@@ -42,44 +49,62 @@ def fit_sine_and_linear(x, y, w):
     ys = A[0]*np.sin(w*x) + A[1]*np.cos(w*x) + A[2]*x + A[3]
     return ys, A
 
-# this is the main function that takes BJDs and converts to UTCs
-def bjd2utc(bjd):
-    A = np.genfromtxt("t_coeffs.txt")
+def bjd2utc(bjd, fnames):
+    """
+    # `bjd2utc`
+
+    Convert BJD time to spacecraft UTC time. Fits a sinusoid with a period of
+    372(?) days and linear trend to the difference between BJD and UTC time
+    """
+
     w = 2*np.pi/372
+    t_bjd, diff, dt = get_times(fnames)
+    y, A = fit_sine_and_linear(t_bjd, diff, w)
     return bjd + A[0]*np.sin(w*bjd) + A[1]*np.cos(w*bjd) + A[2]*bjd + A[3]
+
+def real_footprint(t, fnames):
+    """
+    # `real_footprint`
+
+    Takes real Kepler time values for a certain target.
+    fnames is a list of filenames for all available long-cadence fits files.
+    Fits a sinusoid with a period of 372(?) days and linear trend to the
+    difference between BJD and UTC time for that star's header values and
+    returns the spacecraft-UTC start, stop and centre times.
+    """
+
+    w = 2*np.pi/372
+    t_bjd, diff, dt = get_times(fnames)
+    y, A = fit_sine_and_linear(t_bjd, diff, w)
+    stops = bjd2utc(t, fnames)
+    starts = stops - dt
+    centers = stops - .5*dt
+    return starts, stops, centers
 
 if __name__ == "__main__":
 
     D = "/Users/angusr/angusr/data2"
-    kid = "7341231"
-
-    w = 2*np.pi*1./372
-    t_bjd, diff = get_times(D, kid)  # find all recorded times in headers
-    y, A = fit_sine_and_linear(t_bjd, diff, w)  # fit sin+cos + linear trend
-    np.savetxt("t_coeffs.txt", np.transpose((A)))
-
-    # plot difference between UTC and BJD
-    plt.clf()
-    plt.plot(t_bjd, diff, "k.")
-    xs = np.linspace(t_bjd[0], t_bjd[-1], 1000)
-    ys = A[0]*np.sin(w*xs) + A[1]*np.cos(w*xs) + A[2]*xs + A[3]
-    plt.plot(xs, ys)
-    plt.xlabel("BJD - 2454833")
-    plt.ylabel("BJD - UTC (days)")
-    plt.savefig("test")
+    kid = "7341231"  # a kepler target with lc and sc data chosen at `random'
+    fnames = []
+    qs = range(17)
+    x = []
+    for q in qs:
+        fnames.append(glob.glob("%s/Q%s_public/kplr%s-*_llc.fits"
+                      % (D, q, kid.zfill(9)))[0])
 
     # load test fits file
-    hdulist = pyfits.open("%s/Q16_public/kplr%s-2013098041711_llc.fits"
-                          % (D, kid.zfill(9)))
-    tbdata = hdulist[1].data
-    x = tbdata["TIME"]
+    for fname in fnames:
+        hdulist = pyfits.open(fname)
+        tbdata = hdulist[1].data
+        x.extend(tbdata["TIME"])
 
     # convert BJDs to UTCs
-    utc = bjd2utc(x)
+    x = np.array(x) + 2454833
+    starts, stops, centers = real_footprint(x, fnames)
 
     # plot correction
     plt.clf()
-    plt.plot(x, x-utc, "k.")
+    plt.plot(x, x-stops, "k.")
     plt.xlabel("BJD")
     plt.ylabel("BJD-UTC (days)")
-    plt.savefig("demonstrate_q16")
+    plt.savefig("demonstrate")

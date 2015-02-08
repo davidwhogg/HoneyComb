6fa37a2533b636ef6bda14f1c91a89f9b92f116d
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-02-08 15:07:53 -0500

    found a mode in the poster child

--- pwnnyquist/fft.py
+++ pwnnyquist/fft.py
@@ -1,26 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pyfits
-import glob
 import nufft
-from params import plot_params
-reb = plot_params()
-import superpgram as spg
-from colours import plot_colours
-c = plot_colours()
-import scipy.signal as sps
 import superpgram as spg
 from header_time import real_footprint, real_footprint_sc
-import george
-from george.kernels import ExpSquaredKernel
 import glob
+from gatspy.periodic import LombScargle
 
 # load KASOC light curves
 def corrected_data(kid):
     D = "/Users/angusr/Python/HoneyComb/pwnnyquist/KASOC"
     fnames = glob.glob("%s/kplr%s*.dat" % (D, kid.zfill(9)))
     x, y, yerr = [], [], []
-    for fname in fnames:
+    for fname in fnames[:3]:
         data = np.genfromtxt(fname, skip_header=9).T
         x.extend(data[0])
         y.extend(data[5])
@@ -48,20 +40,20 @@ def load_data(kid, sc):
     else:
         fnames = glob.glob("%s/kplr%s-*_llc.fits" % (D, kid.zfill(9)))
     q, x, y, yerr = [], [], [], []
-    for fname in fnames[:3]:
+    for fname in fnames:
         print fname
         hdulist = pyfits.open(fname)
         tbdata = hdulist[1].data
         x.extend(tbdata["TIME"])
         data = tbdata["PDCSAP_FLUX"]
         q.extend(tbdata["SAP_QUALITY"])
-#         med = np.median(data[np.isfinite(data)])
-#         y.extend(data/med-1)
-        mean = np.mean(data[np.isfinite(data)])
-        y.extend(data-mean)
+        med = np.median(data[np.isfinite(data)])
+        y.extend(data/med-1)
+#         mean = np.mean(data[np.isfinite(data)])
+#         y.extend(data-mean)
         abs_yerr = tbdata["PDCSAP_FLUX_ERR"]
-#         yerr.extend(abs_yerr/med)
-        yerr.extend(abs_yerr)
+        yerr.extend(abs_yerr/med)
+#         yerr.extend(abs_yerr)
     x, y, yerr, q = np.array(x), np.array(y), np.array(yerr), np.array(q)
     l = np.isfinite(x) * np.isfinite(y) * np.isfinite(yerr) * (q==0)
     x, y, yerr = x[l], y[l], yerr[l]
@@ -82,51 +74,66 @@ def freqs(kid, fs):
 
 if __name__ == "__main__":
 
-#     kid = "8006161"
-    kid = "5515314"
-    # kid = "7103006"
-    # kid = "3427720"
-    # kid = "3632418"
+    kid = "8006161"
+#     kid = "5515314"
+#     kid = "7103006"
+#     kid = "3427720"
+#     kid = "3632418"
 #     kid = "7341231"
 
-#     # pwnnyquist short cadence data
-# #     x, y, yerr, ivar = corrected_data(kid)
+    # pwnnyquist short cadence data
+    x, y, yerr, ivar = corrected_data(kid)
 #     x, y, yerr, ivar = load_data(kid, sc=True)
-# #     fs = np.linspace(0.0002, 0.0005, 1000)  # Hz
-# #     fs = np.linspace(0.00265, 0.00285, 10000)  # Hz
-    fs = np.linspace(0.00001, 0.00025, 10000)  # Hz
-#     ws, fs, truths = freqs(kid, fs)
-#     starts, stops, centres = real_footprint_sc(x)
-#     amp2s = spg.superpgram(starts, stops, y, ivar, ws)
-#
-#     # plot light curve
-#     plt.clf()
-#     plt.subplot(3, 1, 1)
-#     plt.plot(x, y, "k.", alpha=.1)
-#
-#     # plot sc superpgram
-#     plt.subplot(3, 1, 2)
-#     plt.plot(fs, amp2s, "k")
-#     if len(truths):
-#         for truth in truths:
-#             plt.axvline(truth*1e-6, color=c.blue, linestyle="--")
-#     plt.xlim(min(fs), max(fs))
-#
-    # pwnnyquist long cadence data
-    x, y, yerr, ivar = load_data(kid, sc=False)
-    ws, fs, truths = freqs(kid, fs)
-    starts, stops, centres = real_footprint(x)
-    plt.clf()
-    plt.subplot(3, 1, 1)
-    plt.errorbar(x, y, yerr=yerr, fmt="k.", capsize=0)
-#     plt.savefig("test")
-    amp2s = spg.superpgram(starts, stops, y, ivar, ws)
 
-    # plot lc superpgram
-    plt.subplot(3, 1, 3)
-    plt.plot(fs, amp2s, "k")
+#     fs = np.linspace(0.00001, 0.00025, 10000)  # Hz 5515314
+    fs = np.linspace(0.00355, 0.00361, 10000)  # Hz 8006161
+    ws, fs, truths = freqs(kid, fs)
+
+    # plot sc superpgram
+    starts, stops, centres = real_footprint_sc(x)
+    amp2s = spg.superpgram(starts, stops, y, ivar, ws)
+    plt.subplot(4, 1, 1)
+    plt.plot(fs, amp2s, "k", alpha=.3)
     if len(truths):
         for truth in truths:
-            plt.axvline(truth*1e-6, color=c.blue, linestyle="--")
+            plt.axvline(truth*1e-6, color=".7", linestyle="-.")
     plt.xlim(min(fs), max(fs))
+    ax = plt.gca()
+    ax.set_yticklabels([])
+    plt.ylabel("sc spg")
+
+    # lombscargle short cadence data
+    model = LombScargle().fit(x, y, yerr)
+    period = 1. / fs
+    power = model.periodogram(period)
+    plt.subplot(4, 1, 2)
+    plt.plot(1./period, power, "k", alpha=.3)
+    if len(truths):
+        for truth in truths:
+            plt.axvline(truth*1e-6, color=".7", linestyle="-.")
+    plt.xlim(min(fs), max(fs))
+    ax = plt.gca()
+    ax.set_yticklabels([])
+    plt.ylabel("sc ls")
+
+    # pwnnyquist long cadence data
+    x, y, yerr, ivar = load_data(kid, sc=False)
+    starts, stops, centres = real_footprint(x)
+    amp2s = spg.superpgram(starts, stops, y, ivar, ws)
+    plt.subplot(4, 1, 3)
+    plt.plot(fs, amp2s, "k", alpha=.3)
+    if len(truths):
+        for truth in truths:
+            plt.axvline(truth*1e-6, color=".7", linestyle="-.")
+    plt.xlim(min(fs), max(fs))
+    ax = plt.gca()
+    ax.set_yticklabels([])
+    plt.ylabel("lc spg")
+
+    # plot light curve
+    plt.subplot(4, 1, 4)
+    plt.plot(x, y, "k.", alpha=.1)
     plt.savefig("fft")
+
+    import os
+    os.system('say "your program has finished"')

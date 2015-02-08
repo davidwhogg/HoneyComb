ffbf8f3c971492f969641b84d3393d7e363d9ed2
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-02-08 15:51:27 -0500

    searching for more modes in the poster child

--- pwnnyquist/fft.py
+++ pwnnyquist/fft.py
@@ -7,9 +7,14 @@ from header_time import real_footprint, real_footprint_sc
 import glob
 from gatspy.periodic import LombScargle
 
+plotpar = {'axes.labelsize': 12,
+           'xtick.labelsize': 12,
+           'ytick.labelsize': 8,
+           'text.usetex': True}
+plt.rcParams.update(plotpar)
+
 # load KASOC light curves
-def corrected_data(kid):
-    D = "/Users/angusr/Python/HoneyComb/pwnnyquist/KASOC"
+def corrected_data(kid, D):
     fnames = glob.glob("%s/kplr%s*.dat" % (D, kid.zfill(9)))
     x, y, yerr = [], [], []
     for fname in fnames[:3]:
@@ -32,16 +37,14 @@ def corrected_data(kid):
     ivar = np.array([i.astype("float64") for i in ivar])
     return x, y, yerr, ivar
 
-def load_data(kid, sc):
+def load_data(kid, D, sc):
     # load sc data
-    D = "/Users/angusr/.kplr/data/lightcurves/%s" % kid.zfill(9)
     if sc == True:
         fnames = glob.glob("%s/kplr%s-*_slc.fits" % (D, kid.zfill(9)))
     else:
         fnames = glob.glob("%s/kplr%s-*_llc.fits" % (D, kid.zfill(9)))
     q, x, y, yerr = [], [], [], []
     for fname in fnames:
-        print fname
         hdulist = pyfits.open(fname)
         tbdata = hdulist[1].data
         x.extend(tbdata["TIME"])
@@ -80,59 +83,69 @@ if __name__ == "__main__":
 #     kid = "3427720"
 #     kid = "3632418"
 #     kid = "7341231"
+    kepler_lc_dir = "/Users/angusr/.kplr/data/lightcurves/%s" % kid.zfill(9)
+    corrected_lc_dir = "/Users/angusr/Python/HoneyComb/pwnnyquist/KASOC"
 
     # pwnnyquist short cadence data
-    x, y, yerr, ivar = corrected_data(kid)
-#     x, y, yerr, ivar = load_data(kid, sc=True)
+    x, y, yerr, ivar = corrected_data(kid, corrected_lc_dir)
+#     x, y, yerr, ivar = load_data(kid, kepler_lc_dir, sc=True)
 
 #     fs = np.linspace(0.00001, 0.00025, 10000)  # Hz 5515314
-    fs = np.linspace(0.00355, 0.00361, 10000)  # Hz 8006161
+    fs = np.arange(0.003, 0.004, 4e-8)  # Hz 8006161
+    print len(fs)
     ws, fs, truths = freqs(kid, fs)
 
     # plot sc superpgram
+    print ("calculating superpgram for short cadence data")
     starts, stops, centres = real_footprint_sc(x)
     amp2s = spg.superpgram(starts, stops, y, ivar, ws)
-    plt.subplot(4, 1, 1)
-    plt.plot(fs, amp2s, "k", alpha=.3)
+    plt.clf()
+    plt.subplot(3, 1, 1)
+    plt.plot(fs, amp2s, "k", alpha=.7)
     if len(truths):
         for truth in truths:
-            plt.axvline(truth*1e-6, color=".7", linestyle="-.")
+            plt.axvline(truth*1e-6, color=".3", linestyle="-.")
     plt.xlim(min(fs), max(fs))
     ax = plt.gca()
     ax.set_yticklabels([])
-    plt.ylabel("sc spg")
+    plt.ylabel("$\mathrm{Short~cadence}$")
 
-    # lombscargle short cadence data
-    model = LombScargle().fit(x, y, yerr)
-    period = 1. / fs
-    power = model.periodogram(period)
-    plt.subplot(4, 1, 2)
-    plt.plot(1./period, power, "k", alpha=.3)
-    if len(truths):
-        for truth in truths:
-            plt.axvline(truth*1e-6, color=".7", linestyle="-.")
-    plt.xlim(min(fs), max(fs))
-    ax = plt.gca()
-    ax.set_yticklabels([])
-    plt.ylabel("sc ls")
+#     # lombscargle short cadence data
+#     print ("calculating Lomb-Scargle for short cadence data")
+#     model = LombScargle().fit(x, y, yerr)
+#     period = 1. / fs
+#     power = model.periodogram(period)
+#     plt.subplot(4, 1, 2)
+#     plt.plot(1./period, power, "k", alpha=.7)
+#     if len(truths):
+#         for truth in truths:
+#             plt.axvline(truth*1e-6, color=".5", linestyle="-.")
+#     plt.xlim(min(fs), max(fs))
+#     ax = plt.gca()
+#     ax.set_yticklabels([])
+#     plt.ylabel("$\mathrm{sc~ls}$")
 
     # pwnnyquist long cadence data
-    x, y, yerr, ivar = load_data(kid, sc=False)
+    print ("calculating superpgram for long cadence data")
+    x, y, yerr, ivar = load_data(kid, kepler_lc_dir, sc=False)
     starts, stops, centres = real_footprint(x)
     amp2s = spg.superpgram(starts, stops, y, ivar, ws)
-    plt.subplot(4, 1, 3)
-    plt.plot(fs, amp2s, "k", alpha=.3)
+    plt.subplot(3, 1, 2)
+    plt.plot(fs, amp2s, "k", alpha=.7)
     if len(truths):
         for truth in truths:
-            plt.axvline(truth*1e-6, color=".7", linestyle="-.")
+            plt.axvline(truth*1e-6, color=".3", linestyle="-.")
     plt.xlim(min(fs), max(fs))
     ax = plt.gca()
     ax.set_yticklabels([])
-    plt.ylabel("lc spg")
+    plt.ylabel("$\mathrm{Long~cadence}$")
 
     # plot light curve
-    plt.subplot(4, 1, 4)
-    plt.plot(x, y, "k.", alpha=.1)
+    plt.subplot(3, 1, 3)
+    plt.plot(x/24./3600, y, "k", alpha=.5)
+    plt.xlabel("$\mathrm{Time~(days)}$")
+    plt.ylabel("$\mathrm{Normalised~flux}$")
+    plt.xlim(min(x), max(x))
     plt.savefig("fft")
 
     import os

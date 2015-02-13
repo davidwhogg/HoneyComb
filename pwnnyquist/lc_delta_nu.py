c109a76c844739709ce6a198bbbcc2a7716652cc
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-02-13 12:18:31 -0500

    higher nu max

--- pwnnyquist/lc_delta_nu.py
+++ pwnnyquist/lc_delta_nu.py
@@ -11,7 +11,7 @@ D = "/n/home11/rangus/Python/HoneyComb/pwnnyquist/data"
 kids = data.iKID
 nm = data.inu_max
 dnu = data.idnu
-l = (nm < 300) * (nm > 0)
+l = (nm < 500) * (nm > 300)
 kids, nm, dnu = kids[l], nm[l], dnu[l]
 
 for i in range(len(kids)):

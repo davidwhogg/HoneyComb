cb419e65ca8db361bbdd028a9c9a058390624ba7
davidwhogg <david.hogg@nyu.edu>
2015-04-24 17:19:23 -0400

    vast improvements; more TESS-like

--- exptime/exptime.py
+++ exptime/exptime.py
@@ -4,12 +4,26 @@ Copyright 2015 David W. Hogg (NYU).
 """
 import numpy as np
 import matplotlib.pyplot as pl
+import os
+import cPickle as pickle
 
 # globals
-ntimes = 70000 # number of data points
+np.random.seed(42)
+ntimes = 1500 # number of data points
 dt = 2. # exposure time of sub-exposures (s)
 n0 = 900 # number of sub-exposures per exposure for S.O.P.
 period = 5.55 * 60. # solar period (s)
+
+def write_to_pickle(fn, stuff):
+    filehandler = open(fn, "wb")
+    pickle.dump(stuff, filehandler)
+    filehandler.close()
+
+def read_from_pickle(fn):
+    filehandler = open(fn, "rb")
+    stuff = pickle.load(filehandler)
+    filehandler.close()
+    return stuff
 
 def make_one_signal(times, exptimes, A, B, omega):
     """
@@ -40,7 +54,7 @@ def make_fake_data(random=True):
     t1s[1:] = t2s[:-1]
     times = 0.5 * (t2s + t1s)
     exptimes = (t2s - t1s)
-    print "make_fake_data(): made", n0, "exposures"
+    print "make_fake_data(): made", len(times), "exposures"
     print times[-5:]
     print exptimes[-5:]
     # make ivars with proper exptime variation; make noise
@@ -93,6 +107,8 @@ def compute_crbs(periods, times, exptimes, ivars):
     nperiod = len(periods)
     crbs = np.zeros((nperiod, 2))
     for ii, P in enumerate(periods):
+        if np.round(2. ** np.round(np.log(ii) / np.log(2.))).astype(int) == ii:
+            print "compute_crbs():", ii
         omega = 2 * np.pi / P
         modelA = make_one_signal(times, exptimes, 1., 0., omega)
         modelB = make_one_signal(times, exptimes, 0., 1., omega)
@@ -100,7 +116,37 @@ def compute_crbs(periods, times, exptimes, ivars):
         crbs[ii, 1] = np.dot(modelB, ivars * modelB)
     return crbs
 
-def plot_stuff(periods, crbs1, crbs2, name1, name2, ylabel, prefix):
+def compute_aliases(period, periods, times, exptimes, ivars):
+    """
+    `compute_aliases`
+
+    Compute, for each period in the `periods` list, the "cosine
+    distance" between the given period onto all other periods.
+
+    Idiotically slow.
+    """
+    omega = 2 * np.pi / period
+    model0A = make_one_signal(times, exptimes, 1., 0., omega)
+    model0B = make_one_signal(times, exptimes, 0., 1., omega)
+    norm0A2 = np.dot(model0A, ivars * model0A)
+    norm0B2 = np.dot(model0B, ivars * model0B)
+    nperiod = len(periods)
+    aliases = np.zeros((nperiod, 2))
+    for ii, P in enumerate(periods):
+        if np.round(2. ** np.round(np.log(ii) / np.log(2.))).astype(int) == ii:
+            print "compute_aliases():", ii
+        omega = 2 * np.pi / P
+        modelA = make_one_signal(times, exptimes, 1., 0., omega)
+        modelB = make_one_signal(times, exptimes, 0., 1., omega)
+        normA2 = np.dot(modelA, ivars * modelA)
+        normB2 = np.dot(modelB, ivars * modelB)
+        aliases[ii, 0] = np.sqrt(np.dot(model0A, ivars * modelA) ** 2 / norm0A2 / normA2 +
+                                 np.dot(model0A, ivars * modelB) ** 2 / norm0B2 / normB2)
+        aliases[ii, 1] = np.sqrt(np.dot(model0B, ivars * modelA) ** 2 / norm0A2 / normA2 +
+                                 np.dot(model0B, ivars * modelB) ** 2 / norm0B2 / normB2)
+    return aliases
+
+def plot_stuff(periods, crbs1, crbs2, name1, name2, ylabel, prefix, logy):
     """
     `plot_stuff`
     """
@@ -122,70 +168,60 @@ def plot_stuff(periods, crbs1, crbs2, name1, name2, ylabel, prefix):
         pl.plot(xs, crbs2, "k-", alpha=0.25, label=name2)
         for xx in vxs:
             pl.axvline(xx, color="r", alpha=0.5)
-            pl.axvline(x0, color="b", alpha=0.5)
+        # pl.axvline(x0, color="b", alpha=0.5)
         pl.legend(loc=2)
         if ii == 0:
-            pl.loglog()
+            if logy:
+                pl.loglog()
+            else:
+                pl.semilogx()
             pl.xlabel("period (s)")
         if ii == 1:
-            pl.semilogy()
+            if logy:
+                pl.semilogy()
             pl.xlabel("frequency (Hz)")
-        big = max(np.max(crbs1), np.max(crbs2))
-        pl.ylim(1.e-8 * big, 1.e1 * big)
         pl.ylabel(ylabel)
+        big = max(np.max(crbs1), np.max(crbs2))
+        if logy:
+            pl.ylim(1.e-8 * big, 1.e1 * big)
+        else:
+            pl.ylim(0., 1.)
+        pl.xlim(min(xs), max(xs))
         pl.savefig("%s%1d.png" % (prefix, ii))
     return None
 
 def plot_crbs(periods, crbs1, crbs2, name1, name2):
     return plot_stuff(periods,
                       np.sum(crbs1, axis=1),
-                      np.sum(crbs2, axis=1), name1, name2, "Cramer-Rao bounds", "crb")
+                      np.sum(crbs2, axis=1),
+                      name1, name2, "Cramer-Rao bounds", "crb", True)
 
 def plot_aliases(periods, aliases1, aliases2, name1, name2):
     return plot_stuff(periods,
-                      np.max(np.abs(aliases1), axis=1),
-                      np.max(np.abs(aliases2), axis=1), name1, name2, "Worst cosine distance", "alias")
-
-def compute_aliases(period, periods, times, exptimes, ivars):
-    """
-    `compute_aliases`
-
-    Compute, for each period in the `periods` list, the "cosine
-    distance" between the given period onto all other periods.
-
-    Idiotically slow.
-    """
-    omega = 2 * np.pi / period
-    model0A = make_one_signal(times, exptimes, 1., 0., omega)
-    model0B = make_one_signal(times, exptimes, 0., 1., omega)
-    norm0A = np.sqrt(np.dot(model0A, ivars * model0A))
-    norm0B = np.sqrt(np.dot(model0B, ivars * model0B))
-    nperiod = len(periods)
-    aliases = np.zeros((nperiod, 4))
-    for ii, P in enumerate(periods):
-        omega = 2 * np.pi / P
-        modelA = make_one_signal(times, exptimes, 1., 0., omega)
-        modelB = make_one_signal(times, exptimes, 0., 1., omega)
-        normA = np.sqrt(np.dot(modelA, ivars * modelA))
-        normB = np.sqrt(np.dot(modelB, ivars * modelB))
-        aliases[ii, 0] = np.dot(model0A, ivars * modelA) / norm0A / normA
-        aliases[ii, 1] = np.dot(model0B, ivars * modelA) / norm0A / normA
-        aliases[ii, 0] = np.dot(model0A, ivars * modelB) / norm0B / normB
-        aliases[ii, 1] = np.dot(model0B, ivars * modelB) / norm0B / normB
-    return aliases
+                      np.max(aliases1, axis=1),
+                      np.max(aliases2, axis=1),
+                      name1, name2, "Worst cosine distance", "alias", False)
 
 if __name__ == "__main__":
-    # df = 1.e-3
-    # periods = 1. / np.arange(0.5 * df, 1.0, df) # (s)
-    dlnp = 0.002
-    periods = np.exp(np.arange(np.log(1.e2) + 0.5 * dlnp, np.log(1.e5), dlnp)) # (s)
-    times1, exptimes1, fluxes1, ivars1 = make_fake_data()
-    crbs1 = compute_crbs(periods, times1, exptimes1, ivars1)
-    aliases1 = compute_aliases(period, periods, times1, exptimes1, ivars1)
+    picklefn = "./exptime.pkl"
+    if not os.path.exists(picklefn):
+        dlnp = 0.05 / ntimes
+        periods = np.exp(np.arange(np.log(3.0e0) + 0.5 * dlnp, np.log(3.0e4), dlnp)) # (s)
+        print "periods", len(periods)
+        times1, exptimes1, fluxes1, ivars1 = make_fake_data()
+        crbs1 = compute_crbs(periods, times1, exptimes1, ivars1)
+        aliases1 = compute_aliases(period, periods, times1, exptimes1, ivars1)
+        times2, exptimes2, fluxes2, ivars2 = make_fake_data(random=False)
+        crbs2 = compute_crbs(periods, times2, exptimes2, ivars2)
+        aliases2 = compute_aliases(period, periods, times2, exptimes2, ivars2)
+        write_to_pickle(picklefn, (periods,
+                                   times1, exptimes1, fluxes1, ivars1, crbs1, aliases1,
+                                   times2, exptimes2, fluxes2, ivars2, crbs2, aliases2))
+    else:
+        (periods,
+         times1, exptimes1, fluxes1, ivars1, crbs1, aliases1,
+         times2, exptimes2, fluxes2, ivars2, crbs2, aliases2) = read_from_pickle(picklefn)
     plot_exptimes(times1, exptimes1, fluxes1, "hogg", "Hogg proposal")
-    times2, exptimes2, fluxes2, ivars2 = make_fake_data(random=False)
-    crbs2 = compute_crbs(periods, times2, exptimes2, ivars2)
-    aliases2 = compute_aliases(period, periods, times2, exptimes2, ivars2)
     plot_exptimes(times2, exptimes2, fluxes2, "TESS", "TESS default")
     plot_crbs(periods, crbs1, crbs2, "Hogg proposal", "TESS default")
     plot_aliases(periods, aliases1, aliases2, "Hogg proposal", "TESS default")
